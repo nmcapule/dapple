7480a5c621d1b8f0ca1b23b5428d910e5b6f72ae
Nathaniel Capule <nmcapule@gmail.com>
2021-08-08 06:15:18 +0000

    feat: omae wa...

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 import { loadYouTubeIframeAPI } from './src/youtube';
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
+function selectYouTubeVideo(): string {
+  const r = Math.random() * 100;
+  if (r < 70.0) {
+    return 'FtutLA63Cp8'; // bad apple
+  }
+  if (r < 97.5) {
+    return 'htIuSWBnmok'; // already dead
+  }
+  return 'dQw4w9WgXcQ'; // you know what this is.
+}
+
 async function initPlayer() {
   await loadYouTubeIframeAPI();
 
@@ -9,14 +18,14 @@ async function initPlayer() {
   const player = new YT.Player('player', {
     height: '390',
     width: '640',
-    videoId: 'FtutLA63Cp8',
+    videoId: selectYouTubeVideo(),
     playerVars: {
       playsinline: 1,
     },
     events: {
       onReady: event => {
         console.log('player is ready');
-        event.target.playVideo();
+        player.playVideo();
       },
       onStateChange: event => {
         if (event.data == YT.PlayerState.PLAYING && !done) {
