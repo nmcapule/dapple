ee7d7dd7936c2b01cde81f889746614c48275c0d
Nathaniel Capule <nmcapule@gmail.com>
2021-08-08 06:31:46 +0000

    feat: Select video and autoplay.

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ transforms to DOM elements.
 Next up is to embed iframes in an SVG element and apply filters to it. It works,
 but I can't do ascii :( The effect still looks good though. Time to learn more
 SVG filtering skills.
+
+## Usage
+
+Just open in browser: https://nmcapule.github.io/dapple/
+
+Play any video you like using query params: `id=<video-id>`. For example:
+
+- https://nmcapule.github.io/dapple/?id=yi-KQ3CKOQU
+
+Enable autoplay with: `autoplay=1`. For example:
+
+- https://nmcapule.github.io/dapple/?id=yi-KQ3CKOQU&autoplay=1
--- index.ts
+++ index.ts
@@ -1,6 +1,15 @@
 import { loadYouTubeIframeAPI } from './src/youtube';
 
+function queryParams(key: string): string {
+  return new URL(window.location.href).searchParams.get(key);
+}
+
 function selectYouTubeVideo(): string {
+  // Check first if param is set.
+  if (queryParams('id')) {
+    return queryParams('id');
+  }
+
   const r = Math.random() * 100;
   if (r < 70.0) {
     return 'FtutLA63Cp8'; // bad apple
@@ -24,8 +33,10 @@ async function initPlayer() {
     },
     events: {
       onReady: event => {
-        console.log('player is ready');
-        player.playVideo();
+        if (queryParams('autoplay')) {
+          console.log('autoplay!');
+          player.playVideo();
+        }
       },
       onStateChange: event => {
         if (event.data == YT.PlayerState.PLAYING && !done) {
